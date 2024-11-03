34395ee3f0a1f560b2c51b3ed9e33e1708d8a678
Marcel Ochs <marcel@ochs.rocks>
2024-11-03 22:16:51 +0100

    fix: fix linking issues when building for emscripten

--- build.zig
+++ build.zig
@@ -1,5 +1,56 @@
 const std = @import("std");
 const rlz = @import("raylib-zig");
+const builtin = @import("builtin");
+
+const emccOutputDir = "zig-out" ++ std.fs.path.sep_str ++ "htmlout" ++ std.fs.path.sep_str;
+const emccOutputFile = "index.html";
+
+pub fn linkWithEmscripten(
+    b: *std.Build,
+    itemsToLink: []const *std.Build.Step.Compile,
+) !*std.Build.Step.Run {
+    // Raylib uses --sysroot in order to find emscripten, so do the same here.
+    if (b.sysroot == null) {
+        @panic("Pass '--sysroot \"[path to emsdk installation]/upstream/emscripten\"'");
+    }
+    const emccExe = switch (builtin.os.tag) {
+        .windows => "emcc.bat",
+        else => "emcc",
+    };
+    var emcc_run_arg = try b.allocator.alloc(u8, b.sysroot.?.len + emccExe.len + 1);
+    defer b.allocator.free(emcc_run_arg);
+
+    emcc_run_arg = try std.fmt.bufPrint(
+        emcc_run_arg,
+        "{s}" ++ std.fs.path.sep_str ++ "{s}",
+        .{ b.sysroot.?, emccExe },
+    );
+
+    // Create the output directory because emcc can't do it.
+    const mkdir_command = b.addSystemCommand(&[_][]const u8{ "mkdir", "-p", emccOutputDir });
+
+    // Actually link everything together.
+    const emcc_command = b.addSystemCommand(&[_][]const u8{emcc_run_arg});
+
+    for (itemsToLink) |item| {
+        emcc_command.addFileArg(item.getEmittedBin());
+        emcc_command.step.dependOn(&item.step);
+    }
+    // This puts the file in zig-out/htmlout/index.html.
+    emcc_command.step.dependOn(&mkdir_command.step);
+    emcc_command.addArgs(&[_][]const u8{
+        "-o",
+        emccOutputDir ++ emccOutputFile,
+        "-sFULL-ES3=1",
+        "-sUSE_GLFW=3",
+        "-sUSE_OFFSET_CONVERTER",
+        "-sASYNCIFY",
+        "-sTOTAL_MEMORY=1024MB", // increase memory
+        "-O3",
+        "--emrun",
+    });
+    return emcc_command;
+}
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -30,7 +81,7 @@ pub fn build(b: *std.Build) !void {
 
         // Note that raylib itself is not actually added to the exe_lib output file,
         // so it also needs to be linked with emscripten.
-        const link_step = try rlz.emcc.linkWithEmscripten(
+        const link_step = try linkWithEmscripten(
             b,
             &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact },
         );
